3f28f0cba16fd9e7b2dc91d8c21ea72f556b210e
Carlo Leonardi <carloleonardi83@gmail.com>
2018-05-28 13:11:20 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,35 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         Calendar date = new GregorianCalendar(1983, 11, 2);
+
         User user = new User("Carlo", "Leonardi", date);
         System.out.println(user.getFiscalCode());
+
+        Calendar date2 = new GregorianCalendar(1983, 3, 22);
+        User user2 = new User("Igor", "Map", date2);
+        System.out.println(user2.getFiscalCode());
+
+        User user3 = new User("John", "Doe", date);
+        System.out.println(user3.getFiscalCode());
+
+        //Parte 0
+        // contemplare +40 in day se Donna
+
+        // Parte1
+        // IGOR GRI
+        // EDER DRE
+        // ALDO DLA
+        // MARIO MRA
+        // ALFIO LFA
+        // JO  JOX
+        // OT  TOX
+
+        //parte2
+        // c1 c3 c4 <- c1 c2 c3
+        // if (!c4) c1 c3 c2
+
+
+
+
     }
 }
--- src/User.java
+++ src/User.java
@@ -12,12 +12,56 @@ public class User {
     }
 
     public String getFiscalCode() {
-        String firstPart = this.name.substring(0,3);
-        String secondPart = this.surname.substring(0,3);
+        String firstPart = this.convert(this.name);
+        String secondPart = this.convert(this.surname);
+
         int year = this.dateOfBirth.get(Calendar.YEAR);
         String yearString = String.valueOf(year);
-        return firstPart + secondPart + yearString.substring(2,4);
+
+        int monthValue = this.dateOfBirth.get(Calendar.MONTH);
+        char month = this.convertMonth(monthValue);
+
+        int dayValue = this.dateOfBirth.get(Calendar.DAY_OF_MONTH);
+        String day = this.convertDay(dayValue);
+
+        return firstPart + secondPart + yearString.substring(2,4) + month + day;
     }
 
+    private String convert(String str) {
+        String result = "";
+        //step 1 uppercase
+        str = str.toUpperCase();
+        //step 2 transform str to Char[]
+        char[] charArray = str.toCharArray();
+        // step3 ciclo for
+        for(int i=0; i < charArray.length; i++) {
+            char letter = charArray[i];
+            //char letter = str.charAt(i);
+            if (!(letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U')) {
+                result += letter;
+            }
+            if (result.length() == 3) {
+                break;
+            }
+        }
+        // step4
+        switch (result.length()){
+            case 2: result += 'X';break;
+            case 1: result += "XX";break;
+            case 0: result += "XXX";break;
+        }
+        return result;
+    }
 
+    private char convertMonth(int month) {
+        return "ABCDEHLMPRST".charAt(month);
+    }
+
+    private String convertDay(int value){
+        String valueInString = String.valueOf(value);
+        if(valueInString.length() == 1){
+            return "0"+valueInString;
+        }
+        return valueInString;
+    }
 }
